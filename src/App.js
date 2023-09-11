35483ba8d9962a7a931bebec38d08c8e31fadaa0
RUSNANDA PURNAMA <rusnandapurnama@gmail.com>
2023-09-11 14:50:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import "boxicons";
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
@@ -15,11 +20,13 @@ function App() {
   }, []);
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
-      </Router>
+      <BrowserRouter basename="/new-portofolios">
+        <Router>
+          <Routes>
+            <Route path="/new-portofolios" element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </BrowserRouter>
     </div>
   );
 }
